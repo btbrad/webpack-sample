e31eee8e03fdb6c85a5eea77bbdcb66a339d9f8d
bt <biantaobrad@gmail.com>
2020-01-05 00:03:06 +0800

    添加样式loader

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
+import './index.css'
+import './test.scss'
+
 const hello = require('./hello')
 document.querySelector('#root').appendChild(hello())
--- webpack.config.js
+++ webpack.config.js
@@ -22,5 +22,17 @@ module.exports = {
       }
     }
   },
-  devtool: 'source-map'
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(scss|sass)/,
+        use: ['style-loader','css-loader','sass-loader']
+      }
+    ]
+  }
 }
