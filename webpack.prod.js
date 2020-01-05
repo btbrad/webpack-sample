6e85c6cc6f5026ab7f37161bc772089e3558ac47
bt <biantaobrad@gmail.com>
2020-01-05 13:22:52 +0800

    清除冗余的css

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,18 @@
+const path = require('path') 
 const common = require('./webpack.common')
 const merge = require('webpack-merge')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const PurifyCssWebpack = require('purifycss-webpack')
+const glob = require('glob')
 
 module.exports = merge(common, {
   devtool: 'source-map',
   plugins: [
     new CleanWebpackPlugin({
       verbose: true  // Write Logs to Console
+    }),
+    new PurifyCssWebpack({
+      paths: glob.sync(path.join(__dirname, 'src/*.html'))
     })
   ]
 })
